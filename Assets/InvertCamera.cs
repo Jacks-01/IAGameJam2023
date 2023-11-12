c890ad437906c4ec9600588d4d6ffd104005c06b
Jack Stubblefield <jack011601@gmail.com>
2023-11-11 19:38:17 -0600

    fixed camera lighting issue

--- Assets/InvertCamera.cs
+++ Assets/InvertCamera.cs
@@ -1,15 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
 public class InvertCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        GetComponent<Camera>().projectionMatrix = GetComponent<Camera>().projectionMatrix * Matrix4x4.Scale(new Vector3(-1, 1, 1));
 
+    new Camera camera;
+
+    public bool flipHorizontal;
+
+    void Awake()
+    {
+        camera = GetComponent<Camera>();
     }
 
+    void OnPreCull()
+    {
+        camera.ResetWorldToCameraMatrix();
+        camera.ResetProjectionMatrix();
+        Vector3 scale = new Vector3(flipHorizontal ? -1 : 1, 1, 1);
+        camera.projectionMatrix = camera.projectionMatrix * Matrix4x4.Scale(scale);
+    }
 
+    void OnPreRender()
+    {
+        GL.invertCulling = flipHorizontal;
+    }
+
+    void OnPostRender()
+    {
+        GL.invertCulling = false;
+    }
 }
